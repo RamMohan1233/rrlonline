bd0283311c0536098a02c471adaa71689421116c
RamMohan1233 <rammohansimham@gmail.com>
2022-10-19 15:47:24 +0530

    changes

---
+++ angular/src/app/model/product.ts
@@ -0,0 +1,12 @@
+export class Product{
+    id:number;
+    name:string;
+    desc:string;
+    customername:string;
+    category:string;
+    actualPrice:number;
+    discount:number;
+    price:number;
+    avail:string;
+    imagepath:string;
+}
